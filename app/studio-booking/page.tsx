341bc1689918a4daf6abc4f38ba898c6387fd950
Matheus <matheusbarcante@hotmail.com>
2026-02-06 09:12:04 -0300

    fix: excluding unused components

--- app/studio-booking/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { StudioBookingForm } from "@/components/studio-booking-form";
-
-export default function StudioBookingPage() {
-    return (
-        <StudioBookingForm studioId="1" />
-    )
-}
